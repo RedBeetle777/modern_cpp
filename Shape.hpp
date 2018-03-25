a873d13aacdc872d4c245be1cb14bac7aa844110
RedBeetle777 <mfaron5@gmail.com>
2018-03-25 11:09:08 +0200

    Shape enum Color and nullptr

--- Shape.hpp
+++ Shape.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
+enum class Color : unsigned char
+{
+    RED,
+    BLUE,
+    GREEN
+};
+
 class Shape
 {
 public:
+    //Color c = Color::RED;
     virtual ~Shape() {}
 
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+    
+protected:
+    Color color = Color::RED;
 };
+
+
+
+
